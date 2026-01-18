b4285ccd38caaef5acfb92fd2d66bf7da238f15b
Sixti2137 <sixti.niescioruk@gmail.com>
2026-01-18 18:49:02 +0100

    Test łączności

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -11,7 +11,7 @@ using System.Windows.Shapes;
 
 namespace QuziLab
 {
-    /// <summary>
+    
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
